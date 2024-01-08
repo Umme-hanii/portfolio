954401e8342244fe5d430b50b051b6dba8d58b7e
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-09 01:18:46 +0530

    Refactor

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,19 +1,8 @@
-"use client";
-
-import React, { useRef } from "react";
+import React from "react";
 import SectionHeading from "./SectionHeading";
-import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
-import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function Experience() {
-  const ref = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["0 1", "1.33 1"],
-  });
-  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
   return (
     <section
       id="experience"

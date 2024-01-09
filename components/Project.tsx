03ba81605e49447e1162cab493ff1c82127dfde0
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-09 20:18:51 +0530

    Link live project

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
+import Link from "next/link";
 
 type ProjectProps = (typeof projectsData)[number];
 
@@ -34,6 +35,13 @@ export default function Project({
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed">{description}</p>
+          <Link
+            href="https://nextjs-apps-kohl.vercel.app/"
+            className="group btn btn-accent max-w-32 my-4 text-neutral-100 px-7 py-3 rounded-lg outline-none focus:scale-110 hover:scale-110 active:scale-105 transition"
+            target="blank"
+          >
+            see live
+          </Link>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
